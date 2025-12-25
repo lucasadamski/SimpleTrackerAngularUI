b23d4df6779b27eb7160d957fe1d7e3614ac4152
Lucas <lucasadams@protonmail.com>
2025-12-25 11:43:43 +0100

    Footer new

--- SimpleTrackerUi/src/app/app.ts
+++ SimpleTrackerUi/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

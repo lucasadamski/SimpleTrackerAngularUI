fec4c6e1177343c996c6f62d24d3efc088788dbf
Lucas <lucasadams@protonmail.com>
2025-12-25 11:34:11 +0100

    navbar component

--- SimpleTrackerUi/src/app/app.ts
+++ SimpleTrackerUi/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

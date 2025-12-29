9c2cde99d10f590a47d284811da8750599545ba3
Lucas <lucasadams@protonmail.com>
2025-12-29 07:43:10 +0100

    Signup page template

---
+++ SimpleTrackerUi/src/app/pages/signup/signup.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css',
+})
+export class Signup {
+
+}
